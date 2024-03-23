e4cfeb23f459ca59211cf0a2ac79a89a697f0cc2
Lllex22 <alexa.bueno@hcdc.edu.ph>
2024-03-23 23:55:36 +0800

    Include changes in src/App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react'
+import React from 'react';
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const generateRandChar = () => {
+  const char = 'abcdefghijk';
+  const randIndex = Math.floor(Math.random() * char.length);
+  return char[randIndex];
 }
 
+const charToNumber = (char) => {
+  return char.charCodeAt(0) - 'a'.charCodeAt(0);
+}
+
+const Table = () => {
+  const [tableData, setTableData] = useState([]);
+  
+  useEffect(() =>{
+    const data = [];
+    for(let i = 0; i < 4; i++){
+      const rowData = [];
+      for(let j = 0; j < 5; j++){
+        rowData.push(generateRandChar());
+      }
+      data.push(rowData);
+    }
+    setTableData(data);
+  }, []);
+  return (
+    <table>
+      <tbody>
+        {tableData.map((row, rowIndex) => (
+          <tr key={rowIndex}>
+              {row.map((char, columnIndex) => (
+                <td
+                  key={`${rowIndex}-${columnIndex}`}
+                  style={{backgroundColor: charToNumber(char) % 2 === 0 ? 'lightgreen' : 'white', color: 'black'}}
+                  >
+                    {char}
+                    
+                </td>
+              ))}
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+const App = () => {
+  return (
+    <div>
+      <h1>Random Character Table</h1>
+      <Table/>
+    </div>
+  )
+}
 export default App;
